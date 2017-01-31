4fd53872cedf170cba4b9bf71d544c6c410fbdba
Benjamin Hafer <bchafer.bch@gmail.com>
2017-01-31 10:25:49 -0500

    just added 'react_on_rails' gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  before_filter :initialize_session
+
+  def initialize_session
+    # Called before any controller method
+    session[:store_index_count] ||= 0
+  end
 end

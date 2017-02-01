fce90701bebce542e7ab6dcb2424520b07761ecc
Benjamin Hafer <bchafer.bch@gmail.com>
2017-01-31 19:34:21 -0500

    changed def current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,9 @@ class ApplicationController < ActionController::Base
   end
   def current_cart
     @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 
   end

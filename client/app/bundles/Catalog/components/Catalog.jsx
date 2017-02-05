d179000c6837ca94d5c330bba7cbdd28d918b715
Benjamin Hafer <bchafer.bch@gmail.com>
2017-02-05 17:15:50 -0500

    just completed 2.2 adding react spa funcitioning correctly

---
+++ client/app/bundles/Catalog/components/Catalog.jsx
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import BookList from './BookList';
+import axios from 'axios';
+const Catalog = React.createClass ({
+    getInitialState: function() {
+        return { books: [] };
+    },
+
+    componentDidMount: function() {
+        var self = this;
+
+        axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
+        axios.get('/')
+            .then(function (response) {
+                console.log(response.data);
+                self.setState({ books: response.data })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <BookList books={this.state.books}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+export default Catalog;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,8 @@ const config = {
     'es5-shim/es5-shim',
     'es5-shim/es5-sham',
     'babel-polyfill',
-    './app/bundles/HelloWorld/startup/registration',
+      './app/bundles/HelloWorld/startup/registration',
+      './app/bundles/Catalog/startup/registration',
   ],
 
   output: {
